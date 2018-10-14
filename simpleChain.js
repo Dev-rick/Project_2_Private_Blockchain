7509d7beaaf17251b7aeb5b1850fffe33288a6d1
Rick Warling <rickwarling@gmail.com>
2018-10-14 13:46:07 +0200

    REFACTOR: Modifying code and simplifying it

--- simpleChain.js
+++ simpleChain.js
@@ -28,26 +28,30 @@ class Block {
 |  ================================================*/
 
 class Blockchain {
-    constructor(blockChainExists = false) {
+    constructor() {
         this.chain = [];
-        if (blockChainExists === false) {
-            this.createGenesisBlock(new Block('Genesis Block - I am the first block on the chain'));
-        }
-        this.getChain();
+        this.getChain().then((dataArray) => {
+            if (dataArray.length === 0) {
+                return this.createGenesisBlock(new Block('Genesis Block - I am the first block on the chain'))
+                    .then(() => {console.log('Genesis block addded');});
+            } else {
+                return;
+            }
+        });
     }
     //adding Genesis Block
     createGenesisBlock(genesisBlock) {
         if (this.chain.length === 0) {
             console.log('Adding the first Block - the Genesis Block!\n');
-            genesisBlock.height = 1;
+            genesisBlock.height = 0;
             genesisBlock.time = new Date().getTime().toString().slice(0, -3);
             genesisBlock.hash = SHA256(JSON.stringify(genesisBlock)).toString();
             console.log(genesisBlock);
-            return addLevelDBData(this.chain.length + 1, JSON.stringify(genesisBlock).toString()).then(() => {
-                this.getChain();
+            return addLevelDBData(this.chain.length, JSON.stringify(genesisBlock).toString()).then(() => {
+
             });
         } else {
-            this.getChain();
+            return;
         }
     }
 
@@ -57,32 +61,32 @@ class Blockchain {
             return new Promise((resolve, reject) => {
                 this.chain = [];
                 this.chain = dataArray;
-                resolve();
+                resolve(dataArray);
             });
         });
     }
 
     // Add new block
     addBlock(newBlock) {
-        console.log('adding following block...\n');
-        this.getChain();
-        if (this.chain.length > 0) {
-            return this.getBlock(this.chain.length).then((value) => {
-                // UTC timestamp
-                newBlock.time = new Date().getTime().toString().slice(0, -3);
-                // Block height
-                newBlock.height = this.chain.length + 1;
-                // previous block hash
-                newBlock.previousBlockHash = JSON.parse(value).hash;
-                newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-                return addLevelDBData(this.chain.length + 1, JSON.stringify(newBlock).toString());
-                // (JSON.parse(this.chain[this.chain.length-1].value).hash);;
-                //Hier verbinden mit previousHash!!!
-            });
-        } else {
-            console.log('Genesis Block is missing\n');
-            return;
-        }
+        this.getChain().then((dataArray) => {
+            if (dataArray.length > 0) {
+                this.getBlock(dataArray.length - 1).then((requestedBlock) => {
+                    console.log('adding following block...\n');
+                    // UTC timestamp
+                    newBlock.time = new Date().getTime().toString().slice(0, -3);
+                    // Block height
+                    newBlock.height = dataArray.length;
+                    // previous block hash
+                    newBlock.previousBlockHash = requestedBlock.hash;
+                    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+                    console.log(newBlock);
+                    return addLevelDBData(dataArray.length, JSON.stringify(newBlock).toString());
+                });
+            } else {
+                console.log('Genesis Block is missing\n');
+                return;
+            }
+        });
     }
 
     // Get block height
@@ -101,7 +105,8 @@ class Blockchain {
     getBlock(blockHeight) {
         return getLevelDBData(blockHeight.toString()).then((value) => {
             return new Promise((resolve) => {
-                resolve(value);
+                let requestedBlock = JSON.parse(value);
+                resolve(requestedBlock);
             });
         });
     }
@@ -110,21 +115,20 @@ class Blockchain {
     validateBlock(blockHeight) {
     // get block object
         return this.getBlock(blockHeight).then((requestedBlock) => {
-            return new Promise((resolve, reject) => {
-                let block = JSON.parse(requestedBlock);
+            return new Promise((resolve) => {
                 // get block hash
-                let blockHash = block.hash;
+                let blockHash = requestedBlock.hash;
                 // remove block hash to test block integrity
-                block.hash = '';
+                requestedBlock.hash = '';
                 // generate block hash
-                let validBlockHash = SHA256(JSON.stringify(block)).toString();
+                let validBlockHash = SHA256(JSON.stringify(requestedBlock)).toString();
                 // Compare
                 if (blockHash === validBlockHash) {
-                    console.log('Block #' + block.height + ' is valid\n');
-                    resolve(block.height);
+                    console.log('Block #' + requestedBlock.height + ' is valid\n');
+                    resolve(true);
                 } else {
                     console.log('Block #' + blockHeight + ' invalid hash:\n' + blockHash + '<>' + validBlockHash);
-                    reject(block.height);
+                    resolve(false);
                 }
             });
         });
@@ -134,73 +138,74 @@ class Blockchain {
     //validate every block on the chain
     validateChain() {
         let PromiseArray = [];
-        return this.getChain().then(() => {
-            for (var i = 1; i <= this.chain.length; i++) {
+        this.getChain().then((dataArray) => {
+            console.log('Checking the content of block = his hash...\n');
+            for (var i = 0; i <= dataArray.length -1; i++) {
                 PromiseArray.push(this.validateBlock(i));
             }
             Promise.all(PromiseArray)
                 .then((results) => {
-                    console.log('Following blocks are valid: ' + results + '\n');
+                    console.log('Now checking the chain...\n');
                 })
-                .catch((e) => {
-                    console.log('Invalid Blocks' + e + '\n');
+                .then(() => {
+                    let promiseErrorLog = [];
+                    for (let i = 0; i < dataArray.length - 1; i++) {
+                        let myErrorPromise = new Promise((resolve, reject) => {
+                            let blockHash = '';
+                            let previousHash = '';
+                            this.getBlock(i).then((requestedBlock) => {
+                                previousHash = requestedBlock.hash;
+                            });
+                            this.getBlock(i + 1).then((requestedBlock) => {
+                                blockHash = requestedBlock.previousBlockHash;
+                                if (blockHash!==previousHash) {
+                                    reject(i);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                        });
+                        promiseErrorLog.push(myErrorPromise);
+                    }
+                    Promise.all(promiseErrorLog)
+                        .then(() => {
+                            console.log('No errors detected');
+                        })
+                        .catch((errorLog) => {
+                            console.log('Block #'+errorLog+ ' makes the chain invalid!\n');
+                        });
                 });
 
         });
+
     }
 }
 
+
 // function to start or add a block on the chain
+
 const startBlockChain = (message) => {
-    return new Promise(function(resolve, reject) {
-        readLevelDBData().then((dataArray) => {
-            let blockChainExists = false;
-            //Checking if Blockchain exists already
-            if (dataArray.length > 0) {
-                console.log('Blockchain exists already!\n');
-                blockChainExists = true;
-                return blockChainExists;
-            } else {
-                return blockChainExists;
-            }
-        }).catch((err) => {
-            console.log('First catch was called' + err + '\n');
-            return;
-        }).then((blockChainExists) => {
-            // Depending on the result before a new Blockchain with or without the GenesisBlock is generated
-            if (!blockChainExists) {
-                console.log('A new blockchain is generated!\n');
-                var myBlockChain = new Blockchain();
-                return myBlockChain;
-            } else {
-                console.log('--> so old Blockchain will be adopted\n');
-                var myBlockChain = new Blockchain(true);
-                return myBlockChain;
-            }
-        }).catch((err) => {
-            console.log('Second Error occured and was catched: ' + err + '\n');
-        }).then((myBlockChain) => {
-            return myBlockChain;
-        }).then((myBlockChain) => {
-            setTimeout(function() {
-                //Adding the Block with the message the user entered
-                let blockTest = new Block('Test Block - ' + (myBlockChain.chain.length + 1) + ' contains message: ' + message + '\n');
-                myBlockChain.addBlock(blockTest).then((result) => {
-                    console.log(JSON.parse(result));
-                    myBlockChain.getChain();
-                    setTimeout((function logBlockchain() {
-                        console.log(myBlockChain);
+    return new Promise((resolve) => {
+        let myBlockchain = new Blockchain();
+        setTimeout(( function addNewBlock() {
+            // myBlockchain.getBlock(4).then((block) => {    // grab a block
+            //     block.hash = 'eddfd5e6c4f2101d110c09e00ee80d46e4b3dbc4bbdee6a1002fe461eae28026';  // parse as JSON
+            //     block.body = 'error';    // modify body
+            //     addLevelDBData(4, JSON.stringify(block));   // reinsert in the same position
+            // });
+            myBlockchain.addBlock(new Block(message));
+            setTimeout((function validateChain() {
+                myBlockchain.validateChain();
+                setTimeout((function logBlockChain() {
+                    myBlockchain.getChain().then(() => {
+                        console.log('Here is your blockchain...\n');
+                        console.log(myBlockchain);
                         process.exit();
-                    }), 10000);
-                });
-            }, 100);
-            // Getting the current height of the chain
-            myBlockChain.getBlockHeight().then((height) => {
-                console.log('Current number of blocks in the chain: ' + height + '\n');
-            });
-            // validate Chain
-            myBlockChain.validateChain();
-        });
+                    });
+                }), 1000);
+            }), 1000);
+        }), 1000);
+
     });
 };
 
